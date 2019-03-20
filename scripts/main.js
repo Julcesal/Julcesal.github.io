c273871e73697f661df4c157c7734b35d405c6b7
Julcesal <48764818+Julcesal@users.noreply.github.com>
2019-03-20 14:52:24 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'Mario Bros &, ' + myName;
 }
 
 if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Mario Bros &, ' + storedName;
 }
 
 myButton.onclick = function() {
